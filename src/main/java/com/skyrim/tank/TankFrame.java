a335cdf40733eac28b26f2f877e5316a5e2215ad
宋凯旋 <songkaixuan@189.cn>
2021-05-23 11:19:52 +0800

    将tank坐标定为变量 再paint()中改变值 paint()被调用后就会动起来

--- src/main/java/com/skyrim/tank/TankFrame.java
+++ src/main/java/com/skyrim/tank/TankFrame.java
@@ -8,6 +8,8 @@ import java.awt.event.WindowEvent;
  * Created by Skyrim on 2021/5/23 10:29
  */
 public class TankFrame extends Frame {
+    int x = 200;
+    int y = 200;
     public TankFrame() throws HeadlessException {
         setSize(800,600);
         setResizable(false);
@@ -23,6 +25,7 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        g.fillRect(200,200,50,50);
+        System.out.println("paint");
+        g.fillRect(x+=10,y+=10,50,50);
     }
 }
