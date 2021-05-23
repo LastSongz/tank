f2f46602bbedc54ca1d1aa9c18544ada4a4533f6
宋凯旋 <songkaixuan@189.cn>
2021-05-23 11:04:28 +0800

    new 一个“窗口”

---
+++ src/main/java/com/skyrim/tank/T.java
@@ -0,0 +1,27 @@
+package com.skyrim.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Created by Skyrim on 2021/5/22 14:10
+ */
+public class T {
+    public static void main(String[] args) {
+//        Frame frame = new Frame();
+//        frame.setSize(800,600);
+//        frame.setResizable(false);
+//        frame.setTitle("TAnk War");
+//        frame.addWindowListener(new WindowAdapter() {
+//            @Override
+//            public void windowClosing(WindowEvent e) {
+//                System.exit(0);
+//            }
+//        });
+//        frame.setVisible(true);
+
+
+        TankFrame frame = new TankFrame();
+    }
+}
